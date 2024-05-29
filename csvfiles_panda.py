d28fa6bebaa4bc949c1b6f70e9ab7f039c63074f
pablodelar <pablo.delar@gmail.com>
2024-05-29 09:27:27 +0100

    first commit

--- csvfiles_panda.py
+++ csvfiles_panda.py
@@ -7,5 +7,21 @@ csv_file_organizations = 'organizations-100.csv'
 # print(os.environ['HOME'])
 # print(os.path.expandvars(os.environ['HOME']))
 
-df = pandas.read_csv(csv_files_path + csv_file_organizations)
-print(type(df))
+
+############################## Reading CSV files with pandas ##############################
+
+df = pandas.read_csv(csv_files_path + csv_file_organizations, index_col= 'Organization Id')
+
+# print(type(df))
+# print(type(df['Number of employees'][0]))
+
+# print(df)
+
+
+############################## Writing CSV files with pandas ##############################
+
+df = pandas.read_csv(csv_files_path + csv_file_organizations,
+                     index_col = 'ID',
+                     header = 0,
+                     names = ['ID', 'Index', 'Company', 'URL', 'Country', 'Description', 'Year', 'Industry', 'No. of Employees'])
+df.to_csv(csv_files_path + 'organizations-100_modified.csv')
